5d178b749b67d97680502050e792c1019407cdb8
海峰 黄 <wonghoifung@haifengdeMacBook-Air.local>
2015-03-29 11:04:55 +0800

    delete useless files

--- scriptbinding/CTestAdapter.cpp
+++
@@ -1,9 +0,0 @@
-#include "CTestAdapter.h"
-
-mfunc_item<CTestAdapter> CTestAdapter::mfuncs[4] = {
-    {"Add", &CTestAdapter::Add},
-    {"getx", &CTestAdapter::getx},
-    {"setx", &CTestAdapter::setx},
-    {NULL, NULL}
-};
-
