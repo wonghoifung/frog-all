5b0b58436dba863f832aeb61956f7744108f73af
wonghoifung <wonghoifung85@gmail.com>
2015-12-19 08:38:15 +0800

    item 5

--- c++11/item5.cpp
+++ c++11/item5.cpp
@@ -1,20 +1,38 @@
 #include <unordered_map>
 #include <iostream>
 
-int main()
+using namespace std;
+
+const std::pair<std::string,int>* getpp(std::unordered_map<std::string,int>& m)
 {
-  std::unordered_map<int,int> m;
-  m[1]=11;
-  m[2]=22;
-  for (const std::pair<int,int>& p:m)
+  const std::pair<std::string,int>* pp = nullptr;
+  for (const std::pair<std::string,int>& p:m)
   {
     // p is not reference within m
-    //p.second += 1;
+    if (p.second == 11) pp = &p;
+  }
+  //cout << __FUNCTION__ << ": " << pp->first << endl;
+  return pp;
+}
+
+int main()
+{
+  std::unordered_map<std::string,int> m;
+  m["1"]=11;
+  m["2"]=22;
+  const std::pair<std::string,int>* pp = nullptr;
+  for (const std::pair<std::string,int>& p:m)
+  //for (const auto& p:m)
+  {
+    // p is not reference within m
+    //if (p.second == 11) pp = &p;
+    ((pair<string,int>&)p).second += 1;
   }
   for (const auto& p:m)
   {
     std::cout<<p.first<<", "<<p.second<<std::endl;
   }
+  cout << __FUNCTION__ << ": " << getpp(m)->first << endl;
   return 0;
 }
 
