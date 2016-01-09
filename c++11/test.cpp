de0c7f6a8d1b6ede30c660c808b984f2eb55c7b5
wonghoifung <wonghoifung85@gmail.com>
2016-01-09 11:13:00 +0800

    decay copy

---
+++ c++11/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <thread>
+
+class A {
+public:
+    void foo(int n ) { std::cout << n << std::endl; }
+};
+
+int main()
+{
+    A a;
+
+    //std::thread t1(&A::foo, std::ref(a), 100);
+
+    std::thread t1(&A::foo, &a, 100);
+
+    t1.join();
+    return 0;
+}
