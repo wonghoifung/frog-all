3f9121e0fc0eb6128d66a0a2835687289638ddf4
wonghoifung <wonghoifung85@gmail.com>
2016-07-06 14:24:50 +0800

    .

--- c++03/item40.cpp
+++ c++03/item40.cpp
@@ -3,41 +3,41 @@
 using namespace std;
 
 class base {
-  int i;
+  void* i;
 public:
   virtual ~base() {}
 };
 
 class derived1: public base {
-  int i1;
+  void* i1;
 };
 
 class derived2: public base {
-  int i2;
+  void* i2;
 };
 
 class derived3: public derived1, public derived2 {
-  int i3;
+  void* i3;
 };
 
 class derived4: virtual public base {
-  int i4;
+  void* i4;
 };
 
 class derived5: virtual public base {
-  int i5;
+  void* i5;
 };
 
 class derived6: public derived4, public derived5 {
-  int i6;
+  void* i6;
 };
 
 int main() {
-  cout << sizeof(void*) << endl;
-  cout << sizeof(int) << endl; // 4
-  cout << sizeof(derived3) << endl; // 40
-  cout << sizeof(derived6) << endl; // 48
-  cout << sizeof(derived1) << endl;
-  cout << sizeof(derived4) << endl;
+  cout << "   void* size: " << sizeof(void*) << endl;
+  cout << "    base size: " << sizeof(base) << endl;
+  cout << "derived3 size: " << sizeof(derived3) << endl; 
+  cout << "derived6 size: " << sizeof(derived6) << endl; 
+  cout << "derived1 size: " << sizeof(derived1) << endl;
+  cout << "derived4 size: " << sizeof(derived4) << endl;
 }
 
